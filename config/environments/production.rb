949541c09ca4fa2ac01299b9162b4d0dbddfc51a
naathyn <nathan3k@gmail.com>
2012-09-07 01:05:08 -0500

    SSL production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,24 +1,15 @@
 Socialrails::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests
   config.cache_classes = true
-
-  # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS
   config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
-  # Generate digests for assets URLs
   config.assets.digest = true
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.force_ssl = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -26,9 +17,6 @@ Socialrails::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -53,13 +41,6 @@ Socialrails::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
