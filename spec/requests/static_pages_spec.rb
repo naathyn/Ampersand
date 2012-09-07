62c48f2b80dcae0facd121f7461202e7f447310e
naathyn <nathan3k@gmail.com>
2012-09-06 22:37:01 -0500

    More helpers

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,44 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Social Rails' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'Social Rails') }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
-    before { visit help_path }
+    before { visit root_path }
+    let(:heading)    { 'Social Rails' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Help' }
   end
 
   describe "About page" do
-    before { visit about_path }
+    before { visit root_path }
+    let(:heading)    { 'Social Rails' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About')) }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| About' }
   end
 
   describe "Contact page" do
-    before { visit contact_path }
+    before { visit root_path }
+    let(:heading)    { 'Social Rails' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Contact' }
   end
 end
