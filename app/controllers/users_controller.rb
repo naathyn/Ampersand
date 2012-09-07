9f0a7a7c6beb31b6698b067f1b9a7f516404d42a
naathyn <nathan3k@gmail.com>
2012-09-07 02:25:10 -0500

    Login/out structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = "Thanks for joining and welcome aboard!"
+      sign_in @user
+      flash[:success] = "Thanks for joining and welcome!"
       redirect_to @user
     else
       render 'new'
     end
   end
 end
+
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,12 +31,27 @@ describe "User pages" do
       end
     end
 
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "secret"
         fill_in "Confirmation", with: "secret"
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Thanks') }
       end
 
       it "should create a user" do
