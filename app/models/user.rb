113c81e5ec9b25f95b952701b5471658c0203e62
naathyn <nathan3k@gmail.com>
2012-10-03 20:15:11 -0500

    added hover tip and username in reply fields fixed some syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base 
-attr_accessible :realname, :email, :name, :location, :bio, :password, :password_confirmation
+  attr_accessible :realname, :email, :name, :location, :bio, :password, :password_confirmation
 	
 	has_secure_password
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Socialrails::Application.routes.draw do
     end
   end
   resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts, only: [:create, :destroy]   
   resources :relationships, only: [:create, :destroy]
   resources :messages, only: [:create]
 
