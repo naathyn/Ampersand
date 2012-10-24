8efb9b62464f21a835046ff7505cce4a839dbfda
naathyn <nathan3k@gmail.com>
2012-10-24 11:32:31 -0500

    updated homepage - alpha #2 release

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,6 @@
 class MessagesController < ApplicationController
   before_filter :signed_in_user
 
-  respond_to :html, :js
-
   def create
     @message = current_user.messages.build(params[:message])
     if @message.save
