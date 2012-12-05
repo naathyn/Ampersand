606166965c9e3b5954f6d46318a613fc9052a2f3
naathyn <nathan3k@gmail.com>
2012-12-05 06:16:04 -0600

    getting it to work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121125201152) do
+ActiveRecord::Schema.define(:version => 20121205103702) do
 
   create_table "captchas", :force => true do |t|
     t.integer  "user_id"
