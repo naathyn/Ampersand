b347f7f471f486ef66edfcc3abcd289859191d56
naathyn <nathan3k@gmail.com>
2012-12-23 10:58:09 -0600

    removing remnants of forgotten responsive styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
 
   def full_title(page_title)
     base_title = "&mpersand"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
     end
   end
 end
