f1270ab8f767745f156c56b198e1888ccd897b11
naathyn <nathan3k@gmail.com>
2012-12-30 16:06:47 -0600

    cut back cache and loading nearly 1s from simple method in model, still troubleshooting

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ActiveRecord::Base
   has_many :replies, foreign_key: "to_id", class_name: "Micropost", dependent: :destroy
 
   has_many :fans, foreign_key: "fan_id", class_name: "Opinion", dependent: :destroy
-  has_many :likes, through: :fans
 
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
