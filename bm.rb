caa723ba64c1bcbecef0cd35d51688bf354985cf
naathyn <nathan3k@gmail.com>
2013-01-02 04:30:05 -0600

    experimenting with performance, finishing up with populate.rake

--- bm.rb
+++
@@ -1,8 +0,0 @@
-require 'benchmark'
-a = 200000
-
-Benchmark.bmbm(10) do |b|
-  b.report('any:     ') { a.times do; true if @replies.any?       ; end }
-  b.report('empty:   ') { a.times do; true unless @replies.empty? ; end }
-  b.report('length:  ') { a.times do; true if @replies.length > 1 ; end }
-end
