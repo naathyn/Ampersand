e63e26a0189bcd58033f91b3a957a64fce9a4c3d
naathyn <nathan3k@gmail.com>
2013-03-13 20:08:08 -0500

    a pre rails 3.2.13 bump

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,9 +1,8 @@
 $(function() {
-    $('.page-fade').addClass('faade');
-
     $('.navigation li').each(function() {
         if (window.location.href.indexOf($(this).find('a:first').attr('href')) > -1) {
             $(this).addClass('active').siblings().removeClass('active');
         }
     });
+    $('.page-fade').addClass('faade');
 });
