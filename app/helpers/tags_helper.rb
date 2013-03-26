d6a052a4883391c803814dba81b91bb4f2e67daf
naathyn <nathan3k@gmail.com>
2013-03-26 05:35:40 -0500

    added helper to tags in views

---
+++ app/helpers/tags_helper.rb
@@ -0,0 +1,10 @@
+module TagsHelper
+  def icon_for(tag)
+    if tag.tagging_count > 1
+      icon = '<i class="icon-tags"></i>'
+    else
+      icon = '<i class="icon-tag"></i>'
+    end
+    icon.html_safe
+  end
+end
