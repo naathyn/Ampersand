1bdff1deae823f9f2d7676f46cf5fbf2d47f3b7b
naathyn <nathan3k@gmail.com>
2013-04-12 14:27:13 -0500

    gem bumps

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,7 +2,7 @@ include ApplicationHelper
 
 def sign_in(user)
   visit signin_path
-  fill_in "Userame", with: user.name
+  fill_in "Username", with: user.name
   fill_in "Password", with: user.password
   click_button "Sign in"
   cookies[:remember_token] = user.remember_token
