d82c3926ec3c1438e78b6486dbd42dc119c2a28a
naathyn <nathan3k@gmail.com>
2013-04-30 16:43:07 -0500

    fix break on flash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,6 @@ class PagesController < ApplicationController
         include: User::FEED_EAGER_LOADING)
       @users = Message.find(:all, include: :user).map { |message|
         message.user.realname }.uniq.join ', '
-      @notice = "Last updated on Wednesday April 24th at 12:30 a.m.
-                                  Preparing for v1's completion!"
       @micropost = current_user.microposts.build
     else
       @title = "Sign up now!"
