294ac49cc349bbf9a53e1c139e12e9f440d4e3dc
naathyn <nathan3k@gmail.com>
2013-04-30 16:26:21 -0500

    modified about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,8 @@ class PagesController < ApplicationController
         include: User::FEED_EAGER_LOADING)
       @users = Message.find(:all, include: :user).map { |message|
         message.user.realname }.uniq.join ', '
+      @notice = "Last updated on Wednesday April 24th at 12:30 a.m.
+                                  Preparing for v1's completion!"
       @micropost = current_user.microposts.build
     else
       @title = "Sign up now!"
