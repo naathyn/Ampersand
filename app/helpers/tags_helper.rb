385d5645abae424cac0386788ec15a4392c53c09
naathyn <nathan3k@gmail.com>
2013-05-16 05:41:57 -0500

    modified about page

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,10 +1,6 @@
 module TagsHelper
   def icon_for(tag)
-    if tag.tagging_count > 1
-      icon = "<i class=\"icon-tags\"></i>"
-    else
-      icon = "<i class=\"icon-tag\"></i>"
-    end
+    icon = tag.tagging_count > 1 ? '<i class="icon-tags"></i>' : '<i class="icon-tag"></i>'
     icon.html_safe
   end
 end
